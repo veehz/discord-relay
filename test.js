745d19f99e398e399ccd05ac586202cd52ba957e
veehz <veehzhz@gmail.com>
2021-03-02 19:07:14 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const drelay = require("./index.js");
-
-drelay.channel("816256933036163092");
-drelay.token("NTU2NDY5NDQ0MDc1OTc4NzY2.XqgwyQ.v_EZF41ZMz5KqgxIgYRPya33qn8");
-const Relay = drelay.init();
-
-Relay.events.on("message", (id, type, msg) => {
-  console.log(id, type, msg);
-});
