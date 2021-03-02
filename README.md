5e5881e46278f2acecd7c3a798badb0acb4abf50
Hua Zhi Vee <veehzhz@gmail.com>
2021-03-02 20:29:36 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# discord-relay
-Relays messages via a discord channel
+# Discord-relay
+Relays messages via a discord channel.
+
+## Example Usage
+
+### Config
+```js
+const drelay = require("@veehz/discord-relay");
+
+drelay.config("==DISCORD BOT TOKEN==", "==CHANNEL ID==");
+// OR
+drelay.token("==DISCORD BOT TOKEN==");
+drelay.channel("==CHANNEL ID==");
+```
+
+### Usage
+```js
+async function run(){
+    // Create relay, accepts four optional arguments: id, startMsg, prefix, hideself
+    // id is the code identifying the current instance
+    // startMsg is the message to be fired with the READY instance 
+    // prefix is attached to every event sent
+    // hideSelf (default=false) is explained below.
+    const Relay = await drelay.init();
+
+    // message event: fired every time an event was received, with id, type, msg. An id sent by itself will be ignored if hideSelf is set to true.
+    Relay.events.on("message", (id, type, msg) => {
+        console.log(id, type, msg);
+    });
+
+    // fullMessage event: fired every time an event was received, with whole content without prefix.
+    Relay.events.on("fullMessage", (msg) => {
+        console.log(msg);
+    });
+
+    // send: sends content with event type
+    Relay.send("msg", "my message");
+}
+
+run();
+```
--- index.js
+++ index.js
@@ -8,16 +8,18 @@ class DiscordRelayClient {
   constructor(id, prefix) {
     this.events = new events.EventEmitter();
     this.client = new Discord.Client();
+    this.client.login(token);
     this.id = id;
     this.prefix = prefix;
   }
-  send(type, msg) {
-    client.channels.cache.get(channel).send(`${prefix} ${id} ${type} ${msg}`);
+  send(type, msg="") {
+    console.log(channel);
+    this.client.channels.cache.get(channel).send(`${this.prefix} ${this.id} ${type} ${msg}`);
   }
 }
 
 module.exports = {
-  init(id = null, startMessage = "", prefix = "$EMITTER") {
+  async init(id = null, startMessage = "", prefix = "$EMITTER", hideSelf=false) {
     if (!id) id = Math.floor(Math.random() * 1000000000).toString();
     const thisClient = new DiscordRelayClient(id, prefix);
     if (id.includes(" ")) {
@@ -25,14 +27,7 @@ module.exports = {
         "discord-relay Warning: ID should not include spaces. Proceeding as usual."
       );
     }
-
-    thisClient.client.once("ready", () => {
-      thisClient.client.channels.cache
-        .get(channel)
-        .send(`${prefix} ${id} READY ${startMessage}`);
-      thisClient.events.emit("ready");
-    });
-
+    
     thisClient.client.on("message", async (message) => {
       if (message.channel.id != channel) return;
       if (!message.content.startsWith(prefix)) return;
@@ -42,11 +37,18 @@ module.exports = {
       const thisId = args.shift();
       const type = args.shift();
       const msg = args.join(" ");
-      if (thisId != id) thisClient.events.emit("message", thisId, type, msg);
+      if (thisId != id && hideSelf) thisClient.events.emit("message", thisId, type, msg);
     });
 
-    thisClient.client.login(token);
+    thisClient.client.once("ready", () => {
+      thisClient.client.channels.cache
+        .get(channel)
+        .send(`${prefix} ${id} READY ${startMessage}`);
+      thisClient.events.emit("ready");
+      return thisClient;
+    });
 
+    await events.once(thisClient.client, "ready");
     return thisClient;
   },
 
